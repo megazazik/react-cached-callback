56b10adbd970fdeec0740a4c6569dff550b25048
Lebedev Mikhail <lebedev.mv@tutu.ru>
2020-02-15 12:57:04 +0300

    fix(usegetcallback): fix error when there was no calls of getCallback during render

--- src/test/useGetCallback.spec.tsx
+++ src/test/useGetCallback.spec.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import { mount, ReactWrapper } from 'enzyme';
 import test from 'tape';
-import useGetCallback, { useGetCallbackInner, checkParams } from '../useGetCallback';
+import useGetCallback, {
+	useGetCallbackInner,
+	checkParams
+} from '../useGetCallback';
 
-const TestChild = ({ }: { callbacks: Array<(...args) => any> }) => null;
+const TestChild = ({}: { callbacks: Array<(...args) => any> }) => null;
 
-const TestComponent = ({ callsParams, getKey = (...args) => args[0], usedParams }) => {
+const TestComponent = ({
+	callsParams,
+	getKey = (...args) => args[0],
+	usedParams
+}) => {
 	const createCallback = useGetCallbackInner(
 		(...args) => (...callArgs) => [...args, ...callArgs],
 		getKey,
@@ -13,22 +20,48 @@ const TestComponent = ({ callsParams, getKey = (...args) => args[0], usedParams 
 	);
 
 	return (
-		<TestChild callbacks={callsParams.map((params) => createCallback(...params))} />
+		<TestChild
+			callbacks={callsParams.map(params => createCallback(...params))}
+		/>
 	);
-}
+};
 
-const mountWithCallbacks = ({ calls, getKey = undefined, usedParams = undefined }) =>
-	mount(<TestComponent callsParams={calls} getKey={getKey} usedParams={usedParams} />);
-const getCallbacks = (wrapper: ReactWrapper) => wrapper.children().props().callbacks;
+const mountWithCallbacks = ({
+	calls,
+	getKey = undefined,
+	usedParams = undefined
+}) =>
+	mount(
+		<TestComponent
+			callsParams={calls}
+			getKey={getKey}
+			usedParams={usedParams}
+		/>
+	);
+const getCallbacks = (wrapper: ReactWrapper) =>
+	wrapper.children().props().callbacks;
 
-test('useGetCallback. Params', (t) => {
+test('useGetCallback. No error without calls', t => {
+	try {
+		getCallbacks(mountWithCallbacks({ calls: [] }));
+		t.pass('No errors');
+	} catch (e) {
+		t.error(e);
+	}
+
+	t.end();
+});
+
+test('useGetCallback. Params', t => {
 	const callback1 = getCallbacks(mountWithCallbacks({ calls: [[]] }))[0];
 
 	t.deepEqual(callback1(5), [5], 'Call params');
 	t.deepEqual(callback1(6), [6], 'Call params');
 	t.deepEqual(callback1(6, 7), [6, 7], 'Call params');
 
-	const callbacks2 = getCallbacks(mountWithCallbacks({ calls: [['first'], ['second'], []] }));
+	const callbacks2 = getCallbacks(
+		mountWithCallbacks({ calls: [['first'], ['second'], []] })
+	);
 
 	t.deepEqual(callbacks2[0](3), ['first', 3], 'Create callback params');
 	t.deepEqual(callbacks2[1](6), ['second', 6], 'Create callback params');
@@ -37,8 +70,10 @@ test('useGetCallback. Params', (t) => {
 	t.end();
 });
 
-test('useGetCallback. Caching', (t) => {
-	const wrapper = mountWithCallbacks({ calls: [['first'], ['second'], ['third']] });
+test('useGetCallback. Caching', t => {
+	const wrapper = mountWithCallbacks({
+		calls: [['first'], ['second'], ['third']]
+	});
 	const callbacks1 = getCallbacks(wrapper);
 
 	wrapper.setProps({ callsParams: [['zero'], ['first'], ['second']] });
@@ -56,16 +91,16 @@ test('useGetCallback. Caching', (t) => {
 	t.end();
 });
 
-test('useGetCallback. User params', (t) => {
+test('useGetCallback. User params', t => {
 	const wrapper = mountWithCallbacks({
 		calls: [['first'], ['second']],
-		usedParams: ['p1', 2],
+		usedParams: ['p1', 2]
 	});
 	const callbacks1 = getCallbacks(wrapper);
 
 	wrapper.setProps({
 		callsParams: [['first'], ['second']],
-		usedParams: ['p1', 2],
+		usedParams: ['p1', 2]
 	});
 	const callbacks2 = getCallbacks(wrapper);
 	t.equal(callbacks1[0], callbacks2[0], 'With first key');
@@ -73,7 +108,7 @@ test('useGetCallback. User params', (t) => {
 
 	wrapper.setProps({
 		callsParams: [['first'], ['second']],
-		usedParams: ['p1', 3],
+		usedParams: ['p1', 3]
 	});
 	const callbacks3 = getCallbacks(wrapper);
 	t.notEqual(callbacks2[0], callbacks3[0], 'With first key. Again');
@@ -82,47 +117,29 @@ test('useGetCallback. User params', (t) => {
 	t.end();
 });
 
-test('useGetCallback. checkParams', (t) => {
-	t.deepEqual(
-		checkParams(),
-		[undefined, undefined],
-	);
+test('useGetCallback. checkParams', t => {
+	t.deepEqual(checkParams(), [undefined, undefined]);
 
-	t.deepEqual(
-		checkParams(undefined, undefined),
-		[undefined, undefined],
-	);
+	t.deepEqual(checkParams(undefined, undefined), [undefined, undefined]);
 
-	t.deepEqual(
-		checkParams(1, undefined),
-		[1, undefined],
-	);
+	t.deepEqual(checkParams(1, undefined), [1, undefined]);
 
-	t.deepEqual(
-		checkParams({ index: 0 }, undefined),
-		[{ index: 0 }, undefined],
-	);
+	t.deepEqual(checkParams({ index: 0 }, undefined), [
+		{ index: 0 },
+		undefined
+	]);
 
 	const getKeyMock = () => '';
-	t.deepEqual(
-		checkParams(getKeyMock, undefined),
-		[getKeyMock, undefined],
-	);
+	t.deepEqual(checkParams(getKeyMock, undefined), [getKeyMock, undefined]);
 
-	t.deepEqual(
-		checkParams(1, ['t']),
-		[1, ['t']],
-	);
+	t.deepEqual(checkParams(1, ['t']), [1, ['t']]);
 
-	t.deepEqual(
-		checkParams(['t']),
-		[undefined, ['t']],
-	);
+	t.deepEqual(checkParams(['t']), [undefined, ['t']]);
 
 	t.end();
 });
 
-test('useGetCallback. Using getKey', (t) => {
+test('useGetCallback. Using getKey', t => {
 	const GetKeyTestComponent = ({ callsParams }) => {
 		const createCallback = useGetCallback(
 			(...args) => (...callArgs) => [...args, ...callArgs],
@@ -130,9 +147,11 @@ test('useGetCallback. Using getKey', (t) => {
 		);
 
 		return (
-			<TestChild callbacks={callsParams.map((params) => createCallback(...params))} />
+			<TestChild
+				callbacks={callsParams.map(params => createCallback(...params))}
+			/>
 		);
-	}
+	};
 
 	const wrapper = mount(
 		<GetKeyTestComponent
@@ -140,22 +159,34 @@ test('useGetCallback. Using getKey', (t) => {
 				[11, false, 'k1'],
 				[5, true, 'k2'],
 				[7, 33, 'k2'],
-				[7, 33, 'k2'],
+				[7, 33, 'k2']
 			]}
 		/>
 	);
 
 	const callbacks1 = getCallbacks(wrapper);
 
-	t.notEqual(callbacks1[0], callbacks1[1], 'Should not be equals with the same key');
-	t.notEqual(callbacks1[1], callbacks1[2], 'Should not be equals with the same key and different params');
-	t.equal(callbacks1[2], callbacks1[3], 'Should be equals with the same key and same params');
+	t.notEqual(
+		callbacks1[0],
+		callbacks1[1],
+		'Should not be equals with the same key'
+	);
+	t.notEqual(
+		callbacks1[1],
+		callbacks1[2],
+		'Should not be equals with the same key and different params'
+	);
+	t.equal(
+		callbacks1[2],
+		callbacks1[3],
+		'Should be equals with the same key and same params'
+	);
 
 	wrapper.setProps({
 		callsParams: [
 			[11, false, 'k1'],
 			[7, 33, 'k2'],
-			[8, 33, 'k2'],
+			[8, 33, 'k2']
 		]
 	});
 
@@ -167,7 +198,7 @@ test('useGetCallback. Using getKey', (t) => {
 	t.end();
 });
 
-test('useGetCallback. used values', (t) => {
+test('useGetCallback. used values', t => {
 	const GetKeyTestComponent = ({ callsParams, usedValues }) => {
 		const createCallback = useGetCallback(
 			(...args) => (...callArgs) => [...args, ...callArgs],
@@ -175,15 +206,17 @@ test('useGetCallback. used values', (t) => {
 		);
 
 		return (
-			<TestChild callbacks={callsParams.map((params) => createCallback(...params))} />
+			<TestChild
+				callbacks={callsParams.map(params => createCallback(...params))}
+			/>
 		);
-	}
+	};
 
 	const wrapper = mount(
 		<GetKeyTestComponent
 			callsParams={[
 				['k1', 2],
-				['k2', 3],
+				['k2', 3]
 			]}
 			usedValues={[1]}
 		/>
@@ -194,32 +227,47 @@ test('useGetCallback. used values', (t) => {
 	wrapper.setProps({
 		callsParams: [
 			['k1', 2],
-			['k2', 3],
+			['k2', 3]
 		],
 		usedValues: [1]
 	});
 
 	const callbacks2 = getCallbacks(wrapper);
-	t.equal(callbacks1[0], callbacks2[0], 'Callbacks should be equal before values changed 1');
-	t.equal(callbacks1[1], callbacks2[1], 'Callbacks should be equal before values changed 2');
+	t.equal(
+		callbacks1[0],
+		callbacks2[0],
+		'Callbacks should be equal before values changed 1'
+	);
+	t.equal(
+		callbacks1[1],
+		callbacks2[1],
+		'Callbacks should be equal before values changed 2'
+	);
 
 	wrapper.setProps({
 		callsParams: [
 			['k1', 2],
-			['k2', 3],
+			['k2', 3]
 		],
 		usedValues: [2]
 	});
 
 	const callbacks3 = getCallbacks(wrapper);
-	t.notEqual(callbacks1[0], callbacks3[0], 'Callbacks should not be equal before values changed 1');
-	t.notEqual(callbacks1[1], callbacks3[1], 'Callbacks should not be equal before values changed 2');
+	t.notEqual(
+		callbacks1[0],
+		callbacks3[0],
+		'Callbacks should not be equal before values changed 1'
+	);
+	t.notEqual(
+		callbacks1[1],
+		callbacks3[1],
+		'Callbacks should not be equal before values changed 2'
+	);
 
 	t.end();
 });
 
-
-test('useGetCallback. Create callback args', (t) => {
+test('useGetCallback. Create callback args', t => {
 	const GetKeyTestComponent = ({ callsParams, usedValues }) => {
 		const createCallback = useGetCallback(
 			(...args) => (...callArgs) => [...args, ...callArgs],
@@ -227,15 +275,17 @@ test('useGetCallback. Create callback args', (t) => {
 		);
 
 		return (
-			<TestChild callbacks={callsParams.map((params) => createCallback(...params))} />
+			<TestChild
+				callbacks={callsParams.map(params => createCallback(...params))}
+			/>
 		);
-	}
+	};
 
 	const wrapper = mount(
 		<GetKeyTestComponent
 			callsParams={[
 				['k1', 2],
-				['k2', 3],
+				['k2', 3]
 			]}
 			usedValues={[1]}
 		/>
@@ -246,26 +296,42 @@ test('useGetCallback. Create callback args', (t) => {
 	wrapper.setProps({
 		callsParams: [
 			['k1', 2],
-			['k2', 3],
+			['k2', 3]
 		],
 		usedValues: [1]
 	});
 
 	const callbacks2 = getCallbacks(wrapper);
-	t.equal(callbacks1[0], callbacks2[0], 'Callbacks should be equal before values changed 1');
-	t.equal(callbacks1[1], callbacks2[1], 'Callbacks should be equal before values changed 2');
+	t.equal(
+		callbacks1[0],
+		callbacks2[0],
+		'Callbacks should be equal before values changed 1'
+	);
+	t.equal(
+		callbacks1[1],
+		callbacks2[1],
+		'Callbacks should be equal before values changed 2'
+	);
 
 	wrapper.setProps({
 		callsParams: [
 			['k1', 1],
-			['k2', 2],
+			['k2', 2]
 		],
 		usedValues: [1]
 	});
 
 	const callbacks3 = getCallbacks(wrapper);
-	t.notEqual(callbacks1[0], callbacks3[0], 'Callbacks should not be equal before values changed 1');
-	t.notEqual(callbacks1[1], callbacks3[1], 'Callbacks should not be equal before values changed 2');
+	t.notEqual(
+		callbacks1[0],
+		callbacks3[0],
+		'Callbacks should not be equal before values changed 1'
+	);
+	t.notEqual(
+		callbacks1[1],
+		callbacks3[1],
+		'Callbacks should not be equal before values changed 2'
+	);
 
 	t.end();
 });
--- src/useGetCallback.ts
+++ src/useGetCallback.ts
@@ -4,68 +4,91 @@ import initGetKey, { Params } from './getKey';
 
 const FUNCTION_NAME = 'callback';
 
-export default function useGetCallback<C extends ((...args) => (...args) => any)> (
+export default function useGetCallback<C extends (...args) => (...args) => any>(
 	createCallback: C,
 	params?: Params,
 	usedValues?: any[]
 ): C;
-export default function useGetCallback<C extends ((...args) => (...args) => any)> (
+export default function useGetCallback<C extends (...args) => (...args) => any>(
 	createCallback: C,
 	usedValues?: any[]
 ): C;
-export default function useGetCallback<C extends ((...args) => (...args) => any)> (
+export default function useGetCallback<C extends (...args) => (...args) => any>(
 	createCallback: C,
 	params?: Params | any[],
 	_usedValues?: any[]
 ): C {
 	const [initGetKeyParams, usedValues] = checkParams(params, _usedValues);
-	return useGetCallbackInner(createCallback, initGetKey(initGetKeyParams), usedValues);
+	return useGetCallbackInner(
+		createCallback,
+		initGetKey(initGetKeyParams),
+		usedValues
+	);
 }
 
-export function useGetCallbackInner<C extends ((...args) => (...args) => any)> (
+export function useGetCallbackInner<C extends (...args) => (...args) => any>(
 	createCallback: C,
-	getKey: (...args) => (number | string | symbol),
+	getKey: (...args) => number | string | symbol,
 	usedValues: any[]
 ): C {
 	const callbacksCache = useRef<object>();
 	if (!callbacksCache.current) {
-		callbacksCache.current = {};
+		callbacksCache.current = { [FUNCTION_NAME]: {} };
 	}
 
 	const usedValuesRef = useRef<any[]>();
-	const isNewValues = !arraysEqual(usedValuesRef.current || [], usedValues || []);
+	const isNewValues = !arraysEqual(
+		usedValuesRef.current || [],
+		usedValues || []
+	);
 	usedValuesRef.current = usedValues;
 
 	useEffect(() => {
-        // update old and clear unused callbacks
-        Object.keys(callbacksCache.current[FUNCTION_NAME]).forEach((cacheKey) => {
-            if (callbacksCache.current[FUNCTION_NAME][cacheKey].yetActual) {
-                delete callbacksCache.current[FUNCTION_NAME][cacheKey].yetActual;
-            } else {
-                delete callbacksCache.current[FUNCTION_NAME][cacheKey];
-            }
-        })
-    });
+		// update old and clear unused callbacks
+		Object.keys(callbacksCache.current[FUNCTION_NAME]).forEach(cacheKey => {
+			if (callbacksCache.current[FUNCTION_NAME][cacheKey].yetActual) {
+				delete callbacksCache.current[FUNCTION_NAME][cacheKey]
+					.yetActual;
+			} else {
+				delete callbacksCache.current[FUNCTION_NAME][cacheKey];
+			}
+		});
+	});
 
-    return ((...args) => {
-        let cache4Function = getCache(callbacksCache.current, FUNCTION_NAME, getKey(...args));
+	return ((...args) => {
+		let cache4Function = getCache(
+			callbacksCache.current,
+			FUNCTION_NAME,
+			getKey(...args)
+		);
 
-        if (!cache4Function || isNewValues || !arraysEqual(cache4Function.args, args)) {
-            cache4Function = {};
-			cache4Function.func = (...callArgs) => cache4Function.cached(...callArgs);
+		if (
+			!cache4Function ||
+			isNewValues ||
+			!arraysEqual(cache4Function.args, args)
+		) {
+			cache4Function = {};
+			cache4Function.func = (...callArgs) =>
+				cache4Function.cached(...callArgs);
 			cache4Function.args = args;
-            setCache(callbacksCache.current, FUNCTION_NAME, getKey(...args), cache4Function);
-        }
+			setCache(
+				callbacksCache.current,
+				FUNCTION_NAME,
+				getKey(...args),
+				cache4Function
+			);
+		}
 
 		cache4Function.cached = createCallback.apply(this, args);
 		cache4Function.yetActual = true;
 
-        return cache4Function.func;
-    }) as any;
+		return cache4Function.func;
+	}) as any;
 }
 
-export function checkParams (params?: Params | any[], usedParams?: any[]): [Params, any[]] {
-	return Array.isArray(params)
-		? [undefined, params]
-		: [params, usedParams];
+export function checkParams(
+	params?: Params | any[],
+	usedParams?: any[]
+): [Params, any[]] {
+	return Array.isArray(params) ? [undefined, params] : [params, usedParams];
 }
