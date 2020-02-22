7967257fe1efa09d453cae3e80f9bbc3faa07625
Lebedev Mikhail <lebedev.mv@tutu.ru>
2020-02-22 16:04:29 +0300

    fix: fix react warning during ssr

--- src/useEventCallback.ts
+++ src/useEventCallback.ts
@@ -1,18 +1,27 @@
-import { useRef, useLayoutEffect, useCallback } from 'react';
+import { useRef, useCallback } from 'react';
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
 export default function useEventCallback<C extends (...args) => any>(fn: C): C {
 	const ref = useRef<(...args) => any>(() => {
-		if (typeof console !== 'undefined' && typeof console.warn === 'function') {
-			console.warn('useEventCallback. You should not call an event handler while rendering.');
+		if (
+			typeof console !== 'undefined' &&
+			typeof console.warn === 'function'
+		) {
+			console.warn(
+				'useEventCallback. You should not call an event handler while rendering.'
+			);
 		}
 	});
 
-	useLayoutEffect(() => {
+	useIsomorphicLayoutEffect(() => {
 		ref.current = fn;
 	});
 
-	return useCallback<(...args) => any>((...args) => {
-		const fn = ref.current;
-		return fn(...args);
-	}, [ref]) as C;
+	return useCallback<(...args) => any>(
+		(...args) => {
+			const fn = ref.current;
+			return fn(...args);
+		},
+		[ref]
+	) as C;
 }
--- src/useGetEventCallback.ts
+++ src/useGetEventCallback.ts
@@ -1,54 +1,60 @@
-import { useRef, useLayoutEffect } from 'react';
+import { useRef } from 'react';
 import initGetKey, { Params } from './getKey';
+import { useIsomorphicLayoutEffect } from './useIsomorphicLayoutEffect';
 
 export { Params };
 
-export default function useGetEventCallback<C extends ((...args) => (...args) => any)> (
-    createCallback: C,
-    params?: Params
-): C {
-    return useGetEventCallbackInner(createCallback, initGetKey(params));
-
+export default function useGetEventCallback<
+	C extends (...args) => (...args) => any
+>(createCallback: C, params?: Params): C {
+	return useGetEventCallbackInner(createCallback, initGetKey(params));
 }
 
-function useGetEventCallbackInner<C extends ((...args) => (...args) => any)> (
-    createCallback: C,
-    getCacheKey: (...args) => (number | string | symbol)
+function useGetEventCallbackInner<C extends (...args) => (...args) => any>(
+	createCallback: C,
+	getCacheKey: (...args) => number | string | symbol
 ): C {
-    const callbacksCache = useRef<object>();
-    if (!callbacksCache.current) {
-        callbacksCache.current = {};
-    }
+	const callbacksCache = useRef<object>();
+	if (!callbacksCache.current) {
+		callbacksCache.current = {};
+	}
 
-    useLayoutEffect(() => {
-        // update old and clear unused callbacks
-        Object.keys(callbacksCache.current).forEach((cacheKey) => {
-            if (callbacksCache.current[cacheKey].next) {
-                callbacksCache.current[cacheKey].cached = callbacksCache.current[cacheKey].next;
-                delete callbacksCache.current[cacheKey].next;
-            } else {
-                delete callbacksCache.current[cacheKey];
-            }
-        })
-    });
+	useIsomorphicLayoutEffect(() => {
+		// update old and clear unused callbacks
+		Object.keys(callbacksCache.current).forEach(cacheKey => {
+			if (callbacksCache.current[cacheKey].next) {
+				callbacksCache.current[cacheKey].cached =
+					callbacksCache.current[cacheKey].next;
+				delete callbacksCache.current[cacheKey].next;
+			} else {
+				delete callbacksCache.current[cacheKey];
+			}
+		});
+	});
 
-    return ((...args) => {
-        const cacheKey = getCacheKey(...args);
-        let cache4Function = callbacksCache.current[cacheKey];
+	return ((...args) => {
+		const cacheKey = getCacheKey(...args);
+		let cache4Function = callbacksCache.current[cacheKey];
 
-        if (!cache4Function) {
-            cache4Function = {};
-            cache4Function.cached = () => {
-                if (typeof console !== 'undefined' && typeof console.warn === 'function') {
-                    console.warn('useEventCallbacks. You should not call an event handler while rendering.');
-                }
-            };
-            cache4Function.func = (...callArgs) => cache4Function.cached(...callArgs);
-            callbacksCache.current[cacheKey] = cache4Function;
-        }
+		if (!cache4Function) {
+			cache4Function = {};
+			cache4Function.cached = () => {
+				if (
+					typeof console !== 'undefined' &&
+					typeof console.warn === 'function'
+				) {
+					console.warn(
+						'useEventCallbacks. You should not call an event handler while rendering.'
+					);
+				}
+			};
+			cache4Function.func = (...callArgs) =>
+				cache4Function.cached(...callArgs);
+			callbacksCache.current[cacheKey] = cache4Function;
+		}
 
-        cache4Function.next = createCallback(...args);
+		cache4Function.next = createCallback(...args);
 
-        return cache4Function.func;
-    }) as any;
+		return cache4Function.func;
+	}) as any;
 }
